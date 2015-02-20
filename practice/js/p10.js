e5c91dd99658dae83bb6ebd5dc8ea46965e76158
yutaka-sano <yutaka.sano@dena.com>
2015-02-20 11:34:50 +0900

    modified p10

--- practice/js/p10.js
+++ practice/js/p10.js
@@ -56,31 +56,86 @@ var AudioManager = (function () {
 var MainApp10 = (function () {
     function MainApp10() {
         var _this = this;
+        this.onWindowResize = function () {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+        };
+        //1.カメラ追加
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+        this.camera.position.set(0, 70, 70);
+        //2.シーン追加
+        this.scene = new THREE.Scene();
+        //3.レンダラー追加
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setClearColor(0xffffff);
+        this.renderer.shadowMapEnabled = true;
+        //4.表示コンテナ指定
+        this.container = document.getElementById('container');
+        this.container.appendChild(this.renderer.domElement);
+        //リサイズ処理
+        this.onWindowResize();
+        window.addEventListener("resize", this.onWindowResize, false);
+        //5 オブジェクト追加
+        //光源追加
+        var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+        directionalLight.position.set(0, 100, 30);
+        directionalLight.castShadow = true;
+        this.scene.add(directionalLight);
+        //cube追加
+        var geometry = new THREE.CubeGeometry(40, 40, 40);
+        var material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+        this.cube = new THREE.Mesh(geometry, material);
+        this.cube.position.set(0, 60, 0);
+        this.cube.castShadow = true;
+        this.scene.add(this.cube);
+        //座標軸追加
+        var axis = new THREE.AxisHelper(1000);
+        axis.position.set(0, 0, 0);
+        this.scene.add(axis);
+        //マウス制御機能追加
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.container.addEventListener("mousemove", (function (e) {
+            var mouseX, mouseY;
+            mouseX = e.clientX - 600 / 2;
+            mouseY = e.clientY - 400 / 2;
+        }), false);
         this.audioManager = new AudioManager(function () {
             _this.audioManager.play();
-            _this.render();
+            _this.animate();
         }, this);
         this.canvas = document.getElementById('visualizer');
         this.canvasContext = this.canvas.getContext('2d');
         this.canvas.setAttribute('width', this.audioManager.getAnalyser().frequencyBinCount * 10);
     }
-    MainApp10.prototype.render = function () {
-        var _this = this;
-        console.log("render");
-        //描画前にスペクトラムを取得する
-        var spectrums = this.audioManager.getSpectrum();
+    MainApp10.prototype.update = function () {
+        this.spectrums = this.audioManager.getSpectrum();
+        //this.draw(spectrums);
+    };
+    MainApp10.prototype.draw = function (spectrums) {
         //描画
         //spectrums 0 - 200の範囲
         this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
         for (var i = 0, len = spectrums.length; i < len; i++) {
             this.canvasContext.fillRect(i * 10, 0, 5, spectrums[i]);
         }
-        this.animationId = requestAnimationFrame(function () {
-            _this.render();
-        });
+    };
+    MainApp10.prototype.render = function () {
+        var d = this.spectrums[7];
+        this.cube.position.y = d;
+        this.renderer.render(this.scene, this.camera);
+    };
+    MainApp10.prototype.animate = function () {
+        var _this = this;
+        this.update();
+        this.draw(this.spectrums);
+        requestAnimationFrame(function (e) { return _this.animate(); });
+        this.render();
     };
     return MainApp10;
 })();
 window.addEventListener("load", function (e) {
     var main = new MainApp10();
+    main.animate();
 });
--- src/typescript/p10.ts
+++ src/typescript/p10.ts
@@ -16,7 +16,6 @@ class AudioManager {
 				this.analyser.fftSize = 128;
 				this.analyser.connect(this.audioContext.destination);
 
-
 				//loading audio file
 				this.fileReader.onload = ()=> {
 						//ロード完了後buffer取得開始
@@ -40,7 +39,7 @@ class AudioManager {
 		 * 再生開始
 		 */
 		public play() {
-				if (this.isPlaySound == true){
+				if (this.isPlaySound == true) {
 						this.source.start(0);
 				}
 		}
@@ -65,40 +64,122 @@ class AudioManager {
 		}
 }
 
+declare module THREE {
+		export var OrbitControls;
+}
+
 class MainApp10 {
+		private scene:THREE.Scene;
+		private camera:THREE.PerspectiveCamera;
+		private renderer;
+		private container;
+		private controls;
+		private cube;
 
 		private animationId;
 		private audioManager:AudioManager;
 		private canvas;
 		private canvasContext;
+		private spectrums;
 
 		constructor() {
+
+				//1.カメラ追加
+				this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+				this.camera.position.set(0, 70, 70);
+
+				//2.シーン追加
+				this.scene = new THREE.Scene();
+
+				//3.レンダラー追加
+				this.renderer = new THREE.WebGLRenderer();
+				this.renderer.setPixelRatio(window.devicePixelRatio);
+				this.renderer.setClearColor(0xffffff);
+				this.renderer.shadowMapEnabled = true;
+
+				//4.表示コンテナ指定
+				this.container = document.getElementById('container');
+				this.container.appendChild(this.renderer.domElement);
+				//リサイズ処理
+				this.onWindowResize();
+				window.addEventListener("resize", this.onWindowResize, false);
+
+				//5 オブジェクト追加
+				//光源追加
+				var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+				directionalLight.position.set(0, 100, 30);
+				directionalLight.castShadow = true;
+				this.scene.add(directionalLight);
+				//cube追加
+				var geometry = new THREE.CubeGeometry(40, 40, 40);
+				var material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+				this.cube = new THREE.Mesh(geometry, material);
+				this.cube.position.set(0, 60, 0);
+				this.cube.castShadow = true;
+				this.scene.add(this.cube);
+				//座標軸追加
+				var axis = new THREE.AxisHelper(1000);
+				axis.position.set(0, 0, 0);
+				this.scene.add(axis);
+
+				//マウス制御機能追加
+				this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+				this.container.addEventListener("mousemove", ((e) =>{
+						var mouseX, mouseY;
+						mouseX = e.clientX - 600 / 2;
+						mouseY = e.clientY - 400 / 2;
+				}), false);
+
+
 				this.audioManager = new AudioManager(()=> {
 						this.audioManager.play();
-						this.render()
+						this.animate()
 				}, this);
 
 				this.canvas = document.getElementById('visualizer');
 				this.canvasContext = this.canvas.getContext('2d');
 				this.canvas.setAttribute('width', this.audioManager.getAnalyser().frequencyBinCount * 10);
-
 		}
 
-		private render(){
-				console.log("render")
-				//描画前にスペクトラムを取得する
-				var spectrums = this.audioManager.getSpectrum();
+		private onWindowResize = function() {
+				this.camera.aspect = window.innerWidth / window.innerHeight;
+				this.camera.updateProjectionMatrix();
+				this.renderer.setSize(window.innerWidth, window.innerHeight);
+		}
+
+		private update(){
+				this.spectrums = this.audioManager.getSpectrum();
+				//this.draw(spectrums);
+		}
+
+		private draw(spectrums) {
 				//描画
 				//spectrums 0 - 200の範囲
+
 				this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 				for (var i = 0, len = spectrums.length; i < len; i++) {
 						this.canvasContext.fillRect(i * 10, 0, 5, spectrums[i]);
 				}
-				this.animationId = requestAnimationFrame(()=>{this.render()});
+		}
+
+		private render() {
+				var d = this.spectrums[7];
+				this.cube.position.y = d
+				this.renderer.render(this.scene, this.camera);
+		}
+
+		public animate() {
+				this.update();
+				this.draw(this.spectrums);
+
+				requestAnimationFrame((e)=>
+						this.animate()
+				);
+				this.render();
 		}
 }
 
-
 window.addEventListener("load", (e) => {
 		var main:MainApp10 = new MainApp10();
+		main.animate();
 });
