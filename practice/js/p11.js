f2f95985f37786a940c3da824e69c68d0a8b7be1
yutaka-sano <yutaka.sano@dena.com>
2015-02-23 14:48:11 +0900

    modified p11 add tween test

--- practice/js/p11.js
+++ practice/js/p11.js
@@ -1,3 +1,5 @@
+/// <reference path="DefinitelyTyped/threejs/three.d.ts" />
+/// <reference path="DefinitelyTyped/tween.js/tween.js.d.ts" />
 var MainApp11 = (function () {
     function MainApp11() {
         var _this = this;
@@ -30,7 +32,7 @@ var MainApp11 = (function () {
         directionalLight.castShadow = true;
         this.scene.add(directionalLight);
         //cube追加
-        var geometry = new THREE.CubeGeometry(40, 40, 40);
+        var geometry = new THREE.BoxGeometry(40, 40, 40);
         var material = new THREE.MeshPhongMaterial({ color: 0x0000ff });
         var cube = new THREE.Mesh(geometry, material);
         cube.position.set(0, 60, 0);
@@ -52,6 +54,9 @@ var MainApp11 = (function () {
         document.addEventListener('mousemove', (function (e) {
             _this.onDocumentMouseMove(e);
         }), false);
+        document.addEventListener('mousedown', (function (e) {
+            _this.onClick(e);
+        }), false);
     }
     MainApp11.prototype.onDocumentMouseMove = function (event) {
         event.preventDefault();
@@ -59,7 +64,24 @@ var MainApp11 = (function () {
         this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
         this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
     };
+    MainApp11.prototype.onClick = function (event) {
+        if (this.INTERSECTED === null)
+            return;
+        var tw = new TWEEN.Tween(this.INTERSECTED.position).to({
+            x: 0,
+            y: 0,
+            z: 100
+        }, 2000).easing(TWEEN.Easing.Elastic.Out);
+        tw.start();
+        new TWEEN.Tween(this.INTERSECTED.rotation).to({
+            x: Math.random() * 2 * Math.PI,
+            y: Math.random() * 2 * Math.PI,
+            z: Math.random() * 2 * Math.PI
+        }, 2000).easing(TWEEN.Easing.Elastic.Out).start();
+    };
     MainApp11.prototype.update = function () {
+        //tween.js update
+        TWEEN.update();
         // find intersections
         //mouseとcameraから当たり判定のあるオブジェクトを取得する
         this.raycaster.setFromCamera(this.mouse, this.camera);
--- src/typescript/p11.ts
+++ src/typescript/p11.ts
@@ -1,3 +1,6 @@
+/// <reference path="DefinitelyTyped/threejs/three.d.ts" />
+/// <reference path="DefinitelyTyped/tween.js/tween.js.d.ts" />
+
 declare module THREE {
 		export var OrbitControls;
 }
@@ -41,7 +44,7 @@ class MainApp11 {
 				directionalLight.castShadow = true;
 				this.scene.add(directionalLight);
 				//cube追加
-				var geometry = new THREE.CubeGeometry(40, 40, 40);
+				var geometry = new THREE.BoxGeometry(40, 40, 40);
 				var material = new THREE.MeshPhongMaterial({ color: 0x0000ff });
 				var cube = new THREE.Mesh(geometry, material);
 				cube.position.set(0, 60, 0);
@@ -65,6 +68,9 @@ class MainApp11 {
 				document.addEventListener('mousemove', ((e)=> {
 						this.onDocumentMouseMove(e)
 				}), false);
+				document.addEventListener('mousedown', ((e)=> {
+						this.onClick(e)
+				}), false);
 		}
 
 		private onWindowResize = function () {
@@ -80,7 +86,27 @@ class MainApp11 {
 				this.mouse.y = -( event.clientY / window.innerHeight ) * 2 + 1;
 		}
 
+		private onClick(event) {
+				if (this.INTERSECTED === null) return;
+
+				var tw = new TWEEN.Tween(this.INTERSECTED.position).to({
+						x: 0,
+						y: 0,
+						z: 100 }, 2000)
+					.easing(TWEEN.Easing.Elastic.Out);
+				tw.start()
+
+				new TWEEN.Tween(this.INTERSECTED.rotation).to({
+						x: Math.random() * 2 * Math.PI,
+						y: Math.random() * 2 * Math.PI,
+						z: Math.random() * 2 * Math.PI }, 2000)
+					.easing(TWEEN.Easing.Elastic.Out).start();
+		}
+
 		private update() {
+				//tween.js update
+				TWEEN.update();
+
 				// find intersections
 				//mouseとcameraから当たり判定のあるオブジェクトを取得する
 				this.raycaster.setFromCamera(this.mouse, this.camera);
