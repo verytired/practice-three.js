18a2d084fc1ae2aa3faf68097eccadc766896cfe
yutaka-sano <yutaka.sano@dena.com>
2015-07-02 13:04:38 +0900

    use config.ts

--- src/typescript/p07.ts
+++ src/typescript/p07.ts
@@ -1,18 +1,7 @@
 //定義ファイル
 /// <reference path="DefinitelyTyped/threejs/three.d.ts" />
 
-//extension/shaderを使えるようにする
-declare module THREE {
-    export var OrbitControls;
-    //effect
-    export var EffectComposer;
-    export var RenderPass;
-    export var ShaderPass;
-    export var DotScreenShader;
-    export var DotMatrixShader;
-    export var RGBShiftShader;
-    export var CopyShader;
-}
+/// <reference path="config.ts" />
 
 class MainApp07 {
     private scene:THREE.Scene;
